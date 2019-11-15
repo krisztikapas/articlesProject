0e1b7ff1dca4b75cf922bc2e1213edb63355c6aa
Kriszti <krisztikapas@yahoo.com>
2019-11-15 23:28:00 +0200

    SearchArticles

--- frontend/src/app/articles/articles.component.ts
+++ frontend/src/app/articles/articles.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ApiService } from '../api.service';
 import { ArticleElement } from '../interfaces/ArticleElement';
-import { MatTableDataSource, MatDialog } from '@angular/material';
 import { UpdateArticleComponent } from '../update-article/update-article.component';
+import { MatTableDataSource, MatDialog, MatSort, MatDialogRef, MatSortHeader, MatPaginator } from '@angular/material';
 
 @Component({
   selector: 'app-articles',
@@ -14,6 +14,10 @@ export class ArticlesComponent implements OnInit {
   displayedColumns: string[] = ['title', 'description', 'category', 'date', 'Actions']
   dataSource;
 
+  @ViewChild(MatSort, {static: false})
+   set sort(sort: MatSort) {
+   }
+
 
 constructor(private service: ApiService, private dialog: MatDialog) { }
 
@@ -24,6 +28,10 @@ constructor(private service: ApiService, private dialog: MatDialog) { }
 
       this.dataSource = new MatTableDataSource<ArticleElement>(data as ArticleElement[])
   })
+  }
+
+  applyFilter(filterValue: string){
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
   updateArticle(article){
